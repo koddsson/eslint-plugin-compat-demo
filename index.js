20d080dfa4d2fcee617711589d2d3b0ef4606115
Kristján Oddsson <koddsson@gmail.com>
2023-04-29 19:39:25 +0200

    Create repro

---
+++ index.js
@@ -0,0 +1,22 @@
+const delayedResponses = {
+  delays: [500, 1300, 3500],
+
+  wait(delay) {
+    return new Promise(resolve => {
+      setTimeout(resolve, delay);
+    });
+  },
+
+  async *[Symbol.asyncIterator]() {
+    for (const delay of this.delays) {
+      await this.wait(delay);
+      yield `Delayed response for ${delay} milliseconds`;
+    }
+  },
+};
+
+(async() => {
+  for await (const response of delayedResponses) {
+    console.log(response);
+  }
+})();
